19553e843257e94f13fc3a339ba9589af47c6404
CUONGNGUYEN <nguyendinhcuong260302@gmail.com>
2026-01-10 21:47:49 +0700

    chore: bootstrap backend & heath check API

---
+++ backend/src/main/java/com/socialsphere/backend/SocialsphereBackendApplication.java
@@ -0,0 +1,13 @@
+package com.socialsphere.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialsphereBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialsphereBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/socialsphere/backend/SocialsphereBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialsphere.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialsphereBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
